deb52331a27be2dfa2a665c0732d06445e6821c0
lunagreen705 <vip1201788339241@gmail.com>
2025-01-10 19:06:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -39,9 +39,10 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "nodev4.fypmoon.org",
-        port: 1118,
-        password: "fypmoon.org",
+       identifier: "INZEWORLD.COM (DE)",
+        host: "lava.inzeworld.com",
+        port: 3128,
+        password: "saher.inzeworld.com",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
